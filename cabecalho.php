ec5eb6fccdb7859ad7f22802e3cc93d6d9aea39a
Julio Mendes <juliomendes160@hotmail.com>
2021-08-09 02:43:43 -0300

    Atualizado

--- cabecalho.php
+++ cabecalho.php
@@ -1,8 +1,17 @@
 <header>
-    <ul>
-        <li><a href="index.php">Cadastrar</a></li>
-        <li><a href="conteiner.php">Contêiner</a></li>
-        <li><a href="movimentacao.php">Movimentações</a></li>
-    </ul>
+    <div class="cabecalho">
+        <div class="logo">
+            <img src="logo.jpeg" alt="">
+        </div>
+        <div class="navegacao">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Cadastrar</a></li>
+                    <li><a href="conteiner.php">Contêiner</a></li>
+                    <li><a href="movimentacao.php">Movimentações</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
 
